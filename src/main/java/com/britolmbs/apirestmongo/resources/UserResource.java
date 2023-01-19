99d265decc99cc25e53edd98668e0f4954cc95f9
britolmbs <britolmbs@hotmail.com>
2023-01-19 10:57:24 -0300

    Obtendo um usuário por id e tratamento de error

--- src/main/java/com/britolmbs/apirestmongo/resources/UserResource.java
+++ src/main/java/com/britolmbs/apirestmongo/resources/UserResource.java
@@ -6,11 +6,10 @@ import com.britolmbs.apirestmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,4 +26,11 @@ public class UserResource {
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User  obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
 }
--- src/main/java/com/britolmbs/apirestmongo/services/UserService.java
+++ src/main/java/com/britolmbs/apirestmongo/services/UserService.java
@@ -2,6 +2,7 @@ package com.britolmbs.apirestmongo.services;
 
 import com.britolmbs.apirestmongo.domain.User;
 import com.britolmbs.apirestmongo.repository.UserRepository;
+import com.britolmbs.apirestmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +11,19 @@ import java.util.List;
 @Service
 public class UserService {
 
-   @Autowired
-   private UserRepository repo;
+    @Autowired
+    private UserRepository repo;
 
     public List<User> findAll(){
         return repo.findAll();
     }
+
+    public User findById(String id){
+        User user = repo.findOne(id);
+        if (user == null) {
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+        return user;
+    }
+
 }
