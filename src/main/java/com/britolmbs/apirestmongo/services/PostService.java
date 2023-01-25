2d558daa9990f7ebaeebc99365cdc08da08827e4
britolmbs <britolmbs@hotmail.com>
2023-01-25 10:28:13 -0300

    Consulta com vários critérios e projeto Finalizado

--- src/main/java/com/britolmbs/apirestmongo/services/PostService.java
+++ src/main/java/com/britolmbs/apirestmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.britolmbs.apirestmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -23,4 +24,9 @@ public class PostService {
     }
     public List<Post> findByTitle(String text) {
         return repo.searchTitle(text);    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+       maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+       return repo.fullSearch(text, minDate, maxDate);
+    }
 }
